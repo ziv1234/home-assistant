ed5bf968a5d085ffbfc8a002503dfc49c123bbe4
Daniel Høyer Iversen <Daniel.Iversen@tibber.com>
2019-08-24 11:23:50 +0200

    config

--- homeassistant/components/han_solo/config_flow.py
+++ homeassistant/components/han_solo/config_flow.py
@@ -1,8 +1,8 @@
 """Config flow to configure Han solo component."""
 import voluptuous as vol
 
-from homeassistant import config_entries, data_entry_flow
-from homeassistant.const import (CONF_HOST, CONF_NAME)
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_NAME
 from homeassistant.core import callback
 
 from .const import DOMAIN
@@ -12,12 +12,12 @@ from .const import DOMAIN
 def configured_instances(hass):
     """Return a set of configured SimpliSafe instances."""
     return set(
-        entry.data[CONF_NAME]
-        for entry in hass.config_entries.async_entries(DOMAIN))
+        entry.data[CONF_NAME] for entry in hass.config_entries.async_entries(DOMAIN)
+    )
 
 
 @config_entries.HANDLERS.register(DOMAIN)
-class HansoloFlowHandler(data_entry_flow.FlowHandler):
+class HansoloFlowHandler(config_entries.ConfigFlow):
     """Config flow for Han solo component."""
 
     VERSION = 1
@@ -34,24 +34,22 @@ class HansoloFlowHandler(data_entry_flow.FlowHandler):
         if user_input is not None:
             if user_input[CONF_NAME] not in configured_instances(self.hass):
                 return self.async_create_entry(
-                    title=user_input[CONF_NAME],
-                    data=user_input,
+                    title=user_input[CONF_NAME], data=user_input
                 )
 
-            self._errors[CONF_NAME] = 'name_exists'
+            self._errors[CONF_NAME] = "name_exists"
 
-        return await self._show_config_form(
-            name='HanSolo',
-            hostname='')
+        return await self._show_config_form(name="HanSolo", hostname="")
 
     async def _show_config_form(self, name=None, hostname=None):
         """Show the configuration form to edit configuration."""
         return self.async_show_form(
-            step_id='user',
-            data_schema=vol.Schema({
-                vol.Required(CONF_NAME, default=name): str,
-                vol.Required(CONF_HOST, default=hostname): str,
-            }),
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_NAME, default=name): str,
+                    vol.Required(CONF_HOST, default=hostname): str,
+                }
+            ),
             errors=self._errors,
         )
-
